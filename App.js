5e0ba8200917b98c82ebee2ab2b5037c076cae7d
Leo <leon_fritas@hotmail.com>
2023-05-17 00:11:14 -0400

    Criado combo box na tela principal.

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import {    StyleSheet,
 } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import  React, { useState } from 'react';
+import { Picker } from '@react-native-picker/picker'
 
 
 
@@ -20,29 +21,37 @@ import  React, { useState } from 'react';
 
 export default function App() {
 
-  const [isLogged, setIsLogged] = useState(false)
+//DEFINE SE O USUARIO ESTÁ LOGADO OU NÃO, RETORNA FALSE COMO PADRÃO, AO ALTERAR PARA TRUE A TELA HOME É MOSTRADA
+const [isLogged, setIsLogged] = useState(false)
 
+//FUNÇÃO PRINCIPAL QUE ESTÁ SENDO CARREGADA NO APP
+function Login(  ) {
 
-  function Login() {
-    const acesso = ()=>{
+      const acesso = (  )=>{
       /* FAZER CHAMADA NO BACK-END PARA ACESSO */ 
-      if(matricula == '123' && senha == '123'){
+      if(matricula == '123' && senha == '123' && selectUnidade == false){
         setIsLogged(true)
+        
+      }else{
+        alert('Preencha os campos corretamente.')
       }
  
       }
-
+      //PASSA VALORES DO INPUT DE LOGIN PARA VARIÁVEIS 'matricula' E 'senha' 
       const [ matricula, setMatricula] = useState('')
       const [ senha, setSenha] = useState('')
-      //
+
+      //SELECIONA A UNIDADE NO COMBOBOX, COMO PADRÃO VEM LARANJEIRAS = FALSE, QUALQUER OUTRA UNIDADE RETORNA TRUE
+      const [selectUnidade, setSelectUnidade] = useState(false)
+
+
+      
+      //ANIMAÇÃO INICIAL
       const [larguraLogo, setLarguraLogo] = useState(new Animated.Value(400))
       const [ alturaLogo, setAlturaLogo ] = useState(new Animated.Value(400))
-      //
       const [escritaLogo, setEscritaLogo] = useState(new Animated.Value(0))
       const [input, setInput] = useState(new Animated.Value(0))
-      
-      
-      /* ANIMAÇÃO */
+
       setTimeout(()=>{
       Animated.timing(
       larguraLogo,{
@@ -76,23 +85,43 @@ export default function App() {
       }
       ).start()
       }, 1000)
-      
+      //
+
+      /* CONTAINER PRINCIPAL */ 
       return (
-      /* CONTAINER */ 
       <View style={stylesLogin.container}>
       <StatusBar />
       <View style={{width: '100%', display: 'flex', alignItems:'center'}}>
-        <Animated.Image style={{opacity: escritaLogo, width: 200, height: 55}}  source={require('./assets/images/logoEscritoNLBlack.png')}/>
-        <Animated.Image style={{width: larguraLogo , height: alturaLogo, marginVertical: 20}} source={require('./assets/images/logoNL.png')}/>
+
+          <Animated.Image style={{opacity: escritaLogo, width: 200, height: 55}}  source={require('./assets/images/logoEscritoNLBlack.png')}/>
+          <Animated.Image style={{width: larguraLogo , height: alturaLogo, marginVertical: 20}} source={require('./assets/images/logoNL.png')}/>
+      
       </View>
       <Animated.View style={{width: '100%', display: 'flex', alignItems:'center', opacity: input}}>
-        <TextInput placeholder = "Matrícula" placeholderTextColor={'#fefefe'} style={stylesLogin.textInput} onChangeText={text => setMatricula(text)}/>
-        <TextInput secureTextEntry={true} placeholder = "Senha" placeholderTextColor={'#fefefe'} style={stylesLogin.textInput} onChangeText={text => setSenha(text)}/>
+      
+          <TextInput placeholder = "Matrícula" placeholderTextColor={'#fefefe'} style={stylesLogin.textInput} onChangeText={text => setMatricula(text)}/>
+          <TextInput secureTextEntry={true} placeholder = "Senha" placeholderTextColor={'#fefefe'} style={stylesLogin.textInput} onChangeText={text => setSenha(text)}/>
+          
           <Pressable style={stylesLogin.btnAcesso} onPress={()=>acesso()}>
-            <Text style={stylesLogin.textBtnAcesso}>
-              Acessar
-            </Text>
+
+              <Text style={stylesLogin.textBtnAcesso}>
+                Acessar
+              </Text>
+
           </Pressable>
+          <Text style={{marginTop:30, marginBottom:5, fontWeight:500}}>Unidade:</Text>
+          <Picker selectedValue={selectUnidade}
+                  onValueChange={(itemValue, itemIndex) =>{
+                    setSelectUnidade(itemValue)
+                  }
+                  } style={{ height: 30, width: '50%',  borderRadius:10, backgroundColor:'#F0FFFF', elevation: 3}}>
+                    
+              <Picker.Item label="Laranjeiras" value= {false} />
+              <Picker.Item label="Japiim" value={true} />
+              <Picker.Item label="Manacapuru" value={true} />
+
+          </Picker>
+        
       </Animated.View>
       </View>
       );
@@ -107,6 +136,8 @@ export default function App() {
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -130,7 +161,8 @@ const stylesLogin = StyleSheet.create({
   borderRadius:10,
   paddingLeft: 10,
   // outline: '1px',
-  marginVertical: 5  
+  marginVertical: 5 ,
+  color: 'white'
   },
   btnAcesso:{
   width: '80%',
